5c0590847848457e28e421ab5f92cab02bcb54c3
Jahnavi <gaurjahnavi84@gmail.com>
2026-01-09 18:41:03 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import './../styles/App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
+
